b1049cfd1174693afa65e3dddb0ee892fab8307b
JustFest <80719903+JustFest@users.noreply.github.com>
2023-06-14 14:01:38 +0300

    123

---
+++ config.py
@@ -0,0 +1,22 @@
+import random
+
+'''
+
+     delay - ваше время (от и до) перерыва между кошельками
+     mode  -  0 - ваши сети (из и в какую) ; 1 - поиск нфт по наличию в сети и отправка в выбранную вами сеть
+
+     avax   /   bsc   /   polygon
+
+     chain - из какой сети
+     to - в какую из сетей (выбирается рандомно) - random.choice(['avax','polygon']) либо в конккретную сеть - просто имя сети 'avax'
+
+    '''
+
+delay = (10, 50)
+mode = 0
+chain = 'polygon'          #   avax   /   bsc   /   polygon
+to = 'avax'     #random.choice(['ваша сеть','ваша сеть'])
+
+info = {'avax':('https://snowtrace.io/tx/','https://rpc.ankr.com/avalanche'),
+        'polygon':('https://polygonscan.com/tx/','https://polygon-rpc.com'),
+        'bsc':('https://bscscan.com/tx/','https://bscrpc.com')}
